c85497c01c36ee074f687ed34db8a31b47df051b
Fionn Bracken <fionn.bracken@ucdconnect.ie>
2021-10-30 23:40:52 +0100

    practical 6 added

---
+++ Practical06/README.md
@@ -0,0 +1,3 @@
+# Practical 6
+
+In this practical we write code for multiplying matrices and use makefiles to simplify the development process.
---
+++ Practical06/matrixmult.f90
@@ -0,0 +1,52 @@
+program matrixmult
+    implicit none
+    integer (kind=4), parameter :: n=5, p=3, q=4
+    real (kind=8) :: a(n,p), b(p,q), c(n,q)
+    integer (kind=4) :: i, j, k
+
+    ! Initialise A, B and C matrices
+    do i=1,n
+        do j=1,p
+            a(i,j) = i+j
+        end do
+    end do
+    do i=1,p
+        do j=1,q
+            b(i,j) = i-j
+        end do
+    end do
+    c = 0.0
+
+    ! Perform matrix multiplication
+    do i=1,n
+        do j=1,q
+            do k=1,p
+                c(i,j) = c(i,j) + a(i,k) * b(k,j)
+            end do
+        end do
+    end do
+
+    ! Print out matrices
+    write(6, '(/,a,/,/)') 'This is matrix A'
+    do i=1,n
+        do j=1,p
+            write(6, '(f3.0)', advance='no') a(i,j)
+        end do
+        write(6,*)
+    end do
+    write(6, '(/,a,/,/)') 'This is matrix B'
+    do i=1,p
+        do j=1,q
+            write(6, '(f3.0)', advance='no') b(i,j)
+        end do
+        write(6,*)
+    end do
+    write(6, '(/,a,/,/)') 'This is matrix C'
+    do i=1,n
+        do j=1,q
+            write(6, '(f5.0)', advance='no') c(i,j)
+        end do
+        write(6,*)
+    end do
+
+end program
