e11d897562e09347575bb3f612d09d4dd9a222cc
Fionn Bracken <fionn.bracken@ucdconnect.ie>
2021-11-16 18:14:46 +0000

    added comments for Practical10

--- Practical10/README.md
+++ Practical10/README.md
@@ -2,4 +2,4 @@
 
 This week we look at recursive functions/subroutines.
 
-We create a program that uses to recursion to solve the Tower of Hanoi problem. This would be extremely difficult to implement using imperative methods, but with a recursive algorithm, we are able to solve it in very few lines of code. 
+We create a program that uses to recursion to solve the Tower of Hanoi problem. This would be extremely difficult to implement using iterative methods, but with a recursive algorithm, we are able to solve it in very few lines of code. 
--- Practical10/hanoi.f90
+++ Practical10/hanoi.f90
@@ -2,9 +2,12 @@
 recursive subroutine hanoi(n, source, dest, inter)
     integer (kind=4), intent(in) :: n, source, dest, inter
 
+    ! Base case when there is only 1 disc
     if (n == 1) then
         write(6,fmt='(a,i2,a,i2,a,i2)') 'Moved disc',n,' from ',source,' to ',dest
         return
+
+    ! recursive case, where we call subroutine for n-1 discs
     else
         call hanoi(n-1, source, inter, dest)
         write(6,'(a,i2,a,i2,a,i2)') 'Moved disc',n,' from ',source,' to ',dest
@@ -15,8 +18,12 @@ end subroutine hanoi
 program hanoi_prog
     implicit none
     integer (kind=4) :: h
+
+    ! Ask user for number of discs
     write(6,*) 'Enter number of discs. '
     read(5,*) h
     write(6,'(a,i2,a)') 'Tower of hanoi solution for ',h,' discs:'
+
+    ! Call recursive function
     call hanoi(h,1,3,2)
 end program hanoi_prog
