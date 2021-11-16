0ef65a369750d58624a4e05907d28661e11ff843
Fionn Bracken <fionn.bracken@ucdconnect.ie>
2021-11-16 17:52:14 +0000

    added comments to Practical03

--- Practical03/use_write.f90
+++ Practical03/use_write.f90
@@ -5,6 +5,7 @@ program use_write
     integer(kind=4) :: i1 = 10, i2=100
     real(kind=4) :: f1=1.11, f2=2.2222
 
+! We write to standard output, by specifying the exact formatting we want
     write(6,'(a, i3, i4, a, f5.2, f7.4)') 'Two integers are', i1, i2, ' Two floats', f1, f2
 
 end program use_write
